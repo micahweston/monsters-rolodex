43f8e41694e37381c5c7d609c385cfb929fc98b3
Micah Weston <micah.weston@gmail.com>
2021-10-14 14:25:54 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [ ],
+      searchField: ''
+    };
+  }
+
+  // This allows us to use a component option. that then allows us to fetch from our JSON API
+  // Then we take the response and turn it into JSON, then we set those users within the JSON to our 
+  // monsters this.state empty array.
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  // Use arrow function to bind this to our arrow function so that we can access the .this of the App class.
+  handleChange = (e) => {
+    this.setState( { searchField: e.target.value } )
+  };
+
+  render() {
+    // everything within {} is a JS expression.
+    // className is JSX version of class in HTML. But becasue we have a class in JS we use className
+    // JSX is JS to run HTML
+    // JSX Syntax. The button changes to setState of string to the new message.
+    
+    // we are creating our filteredMonsters based off of the onChange field given by JSX.
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={filteredMonsters}> 
+          
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
